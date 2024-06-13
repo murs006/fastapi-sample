4e3f0135555e92d4298dc05df5ed9b33e54ab08a
murs006 <mk.rishat@gmail.com>
2024-06-13 09:19:13 +0000

    added root func

--- app/main.py
+++ app/main.py
@@ -22,3 +22,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome!"}
